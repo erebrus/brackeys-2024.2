2bd2d5b945b3f102550fbab8528a9bda8422ef14
erebrus <erebrus@gmail.com>
2024-09-11 20:00:05 +0100

    Update character.gd

--- godot/src/characters/character.gd
+++ godot/src/characters/character.gd
@@ -10,10 +10,10 @@ signal dialogue_changed
 @export var name: String = "Unknown"
 @export var portrait: Texture = preload("res://assets/gfx/portraits/person_unknown.png")
 
-@export var name_available:= false
-@export var portrait_available:= false
+@export var name_available:= false # true if you can select name in tree popup
+@export var portrait_available:= false # true if you can select name in tree popup
 
-@export var is_discovered:= false
+@export var is_discovered:= false #set when position is verified on tree
 @export var is_dead:= false
 
 @export var portrait_clues: Array[String]
