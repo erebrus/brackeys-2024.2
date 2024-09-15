2039f556a129eea0228b0dd1eeec3166c9f2d852
AxelfTV <axelf196@gmail.com>
2024-09-15 01:15:16 +0100

    Clues added

--- godot/src/common/types.gd
+++ godot/src/common/types.gd
@@ -47,7 +47,7 @@ const CLUE_COUNTS={
 	Vector2i(1,1):3+5+2+0,
 	Vector2i(1,2):1+3+0+1,
 	Vector2i(1,3):4+1+0+1, #something needs to be fixed here
-	Vector2i(2,1):0+15+11+12, 
+	Vector2i(2,1):0+15+12+12, 
 	Vector2i(3,1):10+12+0+12,
 	Vector2i(4,1):0+10+20+13,
 }
